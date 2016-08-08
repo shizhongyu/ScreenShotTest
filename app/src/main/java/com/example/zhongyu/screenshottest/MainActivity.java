c5f8cd8ee72372ee540e97710eb3bddc6b09cd16
zhongyu <lixudong@vilstart.com>
2016-08-08 13:46:11 +0800

    Android 截长图。

---
+++ app/src/main/java/com/example/zhongyu/screenshottest/MainActivity.java
@@ -0,0 +1,168 @@
+package com.example.zhongyu.screenshottest;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ScrollView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+    ScrollView scrollView;
+    String sdRoot = Environment.getExternalStorageDirectory().getPath();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        scrollView = (ScrollView) findViewById(R.id.scroll);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        shootScrollView(scrollView, "sdRoot");
+
+//        takeScreenshot();
+        super.onWindowFocusChanged(hasFocus);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    /**
+     * 截取scrollview的屏幕
+     * **/
+    public static Bitmap getScrollViewBitmap(ScrollView scrollView,String picpath) {
+        int h = 0;
+        Bitmap bitmap;
+        // 获取listView实际高度
+        for (int i = 0; i < scrollView.getChildCount(); i++) {
+            h += scrollView.getChildAt(i).getHeight();
+            scrollView.getChildAt(i).setBackgroundColor(Color.parseColor("#ffffff"));
+        }
+        Log.d(TAG, "实际高度:" + h);
+        Log.d(TAG, " 高度:" + scrollView.getHeight());
+        // 创建对应大小的bitmap
+        bitmap = Bitmap.createBitmap(scrollView.getWidth(), h,
+                Bitmap.Config.ARGB_8888);
+        final Canvas canvas = new Canvas(bitmap);
+        scrollView.draw(canvas);
+        // 测试输出
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(picpath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (null != out) {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+                out.flush();
+                out.close();
+            }
+        } catch (IOException e) {
+        }
+        Log.d("TAG", "getScrollViewBitmap() returned: " + bitmap.getHeight());
+        return bitmap;
+    }
+
+    private static String TAG = "Listview and ScrollView item 截图:";
+
+    /**
+     *  截图listview
+     * **/
+    public static Bitmap getListViewBitmap(ListView listView,String picpath) {
+        int h = 0;
+        Bitmap bitmap;
+        // 获取listView实际高度
+        for (int i = 0; i < listView.getChildCount(); i++) {
+            h += listView.getChildAt(i).getHeight();
+        }
+        Log.d(TAG, "实际高度:" + h);
+        Log.d(TAG, "list 高度:" + listView.getHeight());
+        // 创建对应大小的bitmap
+        bitmap = Bitmap.createBitmap(listView.getWidth(), h,
+                Bitmap.Config.ARGB_8888);
+        final Canvas canvas = new Canvas(bitmap);
+        listView.draw(canvas);
+        // 测试输出
+        FileOutputStream out = null;
+        try {
+            out = new FileOutputStream(picpath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            if (null != out) {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+                out.flush();
+                out.close();
+            }
+        } catch (IOException e) {
+        }
+        return bitmap;
+    }
+
+    // 程序入口 截取ScrollView
+    public static void shootScrollView(ScrollView scrollView, String picpath) {
+//        ScreenShot.savePic(getScrollViewBitmap(scrollView, picpath), picpath);
+        ScreenShot.saveMyBitmap(getScrollViewBitmap(scrollView, picpath), picpath);
+    }
+
+    // 程序入口 截取ListView
+    public static void shootListView(ListView listView, String picpath) {
+        ScreenShot.savePic(getListViewBitmap(listView,picpath), picpath);
+    }
+
+    private void takeScreenshot() {
+        Date now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
+        try {
+            // image naming and path  to include sd card  appending name you choose for file
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+
+            // create bitmap screen capture
+            View v1 = getWindow().getDecorView().getRootView();
+            v1.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            v1.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            openScreenshot(imageFile);
+        } catch (Throwable e) {
+            // Several error may come out with file handling or OOM
+            e.printStackTrace();
+        }
+    }
+
+    private void openScreenshot(File imageFile) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(imageFile);
+        intent.setDataAndType(uri, "image/*");
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/zhongyu/screenshottest/ScreenShot.java
@@ -0,0 +1,110 @@
+package com.example.zhongyu.screenshottest;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by zhongyu on 8/8/2016.
+ */
+public class ScreenShot {
+
+    // 获取指定Activity的截屏，保存到png文件
+    public static Bitmap takeScreenShot(Activity activity) {
+        // View是你需要截图的View
+        View view = activity.getWindow().getDecorView();
+        view.setDrawingCacheEnabled(true);
+        view.buildDrawingCache();
+        Bitmap b1 = view.getDrawingCache();
+
+        // 获取状态栏高度
+        Rect frame = new Rect();
+        activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+        int statusBarHeight = frame.top;
+        System.out.println(statusBarHeight);
+
+        // 获取屏幕长和高
+        int width = activity.getWindowManager().getDefaultDisplay().getWidth();
+        int height = activity.getWindowManager().getDefaultDisplay()
+                .getHeight();
+        // 去掉标题栏
+        // Bitmap b = Bitmap.createBitmap(b1, 0, 25, 320, 455);
+        Bitmap b = Bitmap.createBitmap(b1, 0, statusBarHeight, width, height
+                - statusBarHeight);
+        view.destroyDrawingCache();
+        return b;
+    }
+
+    // 保存到sdcard
+    public static void savePic(Bitmap b, String strFileName) {
+        FileOutputStream fos = null;
+        Log.d("TAG", "savePic() returned: "  );
+        try {
+            fos = new FileOutputStream(strFileName);
+            if (null != fos) {
+                b.compress(Bitmap.CompressFormat.PNG, 90, fos);
+                Log.d("TAG", "savePic() returned:    " + b.getHeight());
+                fos.flush();
+                fos.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 程序入口 截取当前屏幕
+    public static void shootLoacleView(Activity a, String picpath) {
+        ScreenShot.savePic(ScreenShot.takeScreenShot(a), picpath);
+    }
+
+    /**
+     * 保存bitmap到SD卡
+     * @param bitName 保存的名字
+     * @param mBitmap 图片对像
+     * return 生成压缩图片后的图片路径
+     */
+    @SuppressLint("SdCardPath")
+    public static String saveMyBitmap(Bitmap mBitmap, String bitName) {
+        File f = new File("/sdcard/" + bitName + ".png");
+
+        try {
+            f.createNewFile();
+        } catch (IOException e) {
+            System.out.println("在保存图片时出错：" + e.toString());
+        }
+        FileOutputStream fOut = null;
+        try {
+            fOut = new FileOutputStream(f);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            mBitmap.compress(Bitmap.CompressFormat.PNG, 100, fOut);
+        } catch (Exception e) {
+            return "create_bitmap_error";
+        }
+        try {
+            fOut.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fOut.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "/sdcard/" + bitName + ".png";
+    }
+
+}
